97d49e6d0d3861d7d1b4076b9508f57be746ed8d
Sasa Milosevic <sasmilo20@gmail.com>
2021-01-28 19:15:27 +0100

    Static generator, can not add text or change image

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import MemeGenerator from './MemeGenerator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png"
+          className="App-logo"
+          alt="logo"
+        />{' '}
+        <p>Random meme generator</p>
+        <MemeGenerator />
       </header>
     </div>
   );
