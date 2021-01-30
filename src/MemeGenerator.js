aec470eaecd6a9db677f9e5a0f1de2cb6a52cf5b
Sasa Milosevic <sasmilo20@gmail.com>
2021-01-30 18:39:48 +0100

    Add download function

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,76 +1,102 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import './index.css';
 
-class MemeGenerator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      topText: '', // Initialization of the top text
-      bottomText: '', // Initialization of the bottom text
-      randomImg: 'https://api.memegen.link/images/aag.png', // Initialization of the photo
-      allMemeImgs: [], // Initialization of the allMemeImgs array
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function MemeGenerator() {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [randomImg, setRandomImg] = useState(
+    'https://api.memegen.link/images/aag.png',
+  );
+  const [allMemeImgs, setAllMemeImgs] = useState([]);
 
-  componentDidMount() {
-    // ensure that data is fetched at the beginning
+  useEffect(() => {
     fetch('https://api.memegen.link/templates/') // call to URL
-      // .then((response) => response.json())
+      .then((response) => response.json()) // We say that we want response in a form of JSON
       .then((response) => {
-        // const { arrayOfMemes } = response; // Put the resulting json to the array called arrayOfMemes
-
-        this.setState({ allMemeImgs: response }); // Now allMemeImgs contains an array of objects we fetched
+        setAllMemeImgs(response); // Resulting JSON is the array of objects
+        // Now allMemeImgs contains an array of objects we fetched
       });
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault(); // so it doesn't try to refresh the page
+
+    const randNum = Math.floor(Math.random() * allMemeImgs.length); // Select one random position in the array of objects
+    const randMemeImgObj = allMemeImgs[randNum]; // We got a random object from an array of the objects
+    const randMemeImgUrl = randMemeImgObj.blank; // URL of the randomly chosen object (it's a value tied to a "blank" key)
+
+    setRandomImg(randMemeImgUrl);
   }
+  const topTextNoSpace = topText.replace(/ /g, '_');
+  const bottomTextNoSpace = bottomText.replace(/ /g, '_');
+  const randImgNoExtension = randomImg.substring(0, randomImg.Length - 4);
+  const memeUrl = randImgNoExtension.concat(
+    '/',
+    topTextNoSpace,
+    '/',
+    bottomTextNoSpace,
+    '.png',
+  );
 
-  handleChange(event) {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  }
+  const download = () => {
+    fetch({
+      url: memeUrl,
+      method: 'GET',
+      headers: {},
+    })
+      .then((response) => {
+        response.arrayBuffer().then(function (buffer) {
+          const url = window.URL.createObjectURL(new Blob([buffer]));
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', 'meme.png');
+          document.body.appendChild(link);
+          link.click();
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const randNum = [(Math.random() * this.state.allMemeImgs.length) | 0];
-    // const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length);
-    const randMemeImgObj = this.state.allMemeImgs[randNum]; // We got a random object from an array of the objects
-    const randMemeImg = function () {
-      return randMemeImgObj.blank;
-    };
-    // const randMemeImg = randMemeImgObj.blank;
-    this.setState({ randomImg: randMemeImg });
-  }
+  return (
+    <div>
+      <form className="meme-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="topText"
+          placeholder="Top Text"
+          value={topText}
+          onChange={(e) => setTopText(e.currentTarget.value)}
+        />
+        <input
+          type="text"
+          name="bottomText"
+          placeholder="Bottom Text"
+          value={bottomText}
+          onChange={(e) => setBottomText(e.currentTarget.value)}
+        />
 
-  render() {
-    return (
-      <div>
-        <form className="meme-form" onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="topText"
-            placeholder="Top Text"
-            value={this.state.topText}
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="bottomText"
-            placeholder="Bottom Text"
-            value={this.state.bottomText}
-            onChange={this.handleChange}
-          />
+        <button>New meme</button>
 
-          <button>New photo</button>
-        </form>
-        <div className="meme">
-          <img src={this.state.randomImg} alt="" />
-          <h2 className="top">{this.state.topText}</h2>
-          <h2 className="bottom">{this.state.bottomText}</h2>
-        </div>
+        <button
+          style={{ visibility: memeUrl ? 'visible' : 'hidden' }}
+          className="downloadBtn"
+          onClick={(e) => {
+            download(e);
+          }}
+        >
+          Download
+        </button>
+      </form>
+
+      <div className="meme">
+        <img src={randomImg} alt="" />
+        <h2 className="top">{topText}</h2>
+        <h2 className="bottom">{bottomText}</h2>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default MemeGenerator;
